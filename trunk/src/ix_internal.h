20d40d2aadcd75bd6908e8667f02ecf5ca39d261
wwtmac@gmail.com <wwtmac@gmail.com@2e8724b3-4663-09c7-7dde-4004479e4a0f>
2013-05-21 16:45:48 +0000

    internal header file

---
+++ trunk/src/ix_internal.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   ix_internal.h
+ * Author: wwt
+ *
+ * Created on May 20, 2013, 11:51 PM
+ */
+/// it's for constants and defines 
+#ifndef IX_INTERNAL_H
+#define	IX_INTERNAL_H
+#include <cstdlib>
+#include <cstring>
+#include "ix.h"
+
+#define IX_INVALIDATTR -1
+
+
+
+#endif
+ 
+
+  
+
