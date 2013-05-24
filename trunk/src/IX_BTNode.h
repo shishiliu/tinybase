11414cff73652800fe077ed54c3e7985c8786165
wwtmac@gmail.com <wwtmac@gmail.com@2e8724b3-4663-09c7-7dde-4004479e4a0f>
2013-05-24 15:17:31 +0000

    updating the Btree node definition

--- trunk/src/IX_BTNode.h
+++ trunk/src/IX_BTNode.h
@@ -10,30 +10,42 @@
 #include "redbase.h"
 #include "pf.fwd.h"
 #include "rm_rid.h"
+#include <string.h>
 ////////////////////////////////////////////////////////////////////
 //
 // IX_BTNode: IX B+ Tree interface
 //
+// The leaf node and internal node used the same structure beacause the  internal node do not use 
+// the slot id part of the RID 
 class IX_BTNode {
     friend class IX_FileHandle;
     friend class IX_FileScan;
 public:
-    IX_BTNode(AttrType attributeType, PF_PageHandle& aPh, bool isNewPage, int pageSize);
+    IX_BTNode(AttrType attributeType,int iAttrLength, PF_PageHandle& aPh, bool isNewPage);
     ~IX_BTNode();
 
     // Return the data corresponding to the record.  Sets *pData to the
     // record contents.
-    RC GetData(char *&pData) const;
-
-    // Return the RID associated with the record
-    RC GetRid (RID &rid) const;
-
+    RC GetData(char *&aData) const;
+    PageNum GetLeft();
+    void SetLeft(PageNum aPageId);
+    PageNum GetRight();
+    void SetRight(PageNum aPageId);
+    RC InsertNode(const void* aKey, const RID & aRid);
+    RC DeleteNode(const void* aKey);
+    RC MergeNode(IX_BTNode* aNode);
+    RC SplitNode(IX_BTNode* aNewNode);
+    int GetKeysNum();
+    RC GetKey(int iPos, void * & aKey);
+    RC SetKey(int iPos,const void* aKey);
+    int FindKey(const void* &aKey);
+    int FindKeyExact(const void* &aKey,const RID& aRid);
+    int CompareKey(const void * aKey, const void * bKey);
+    bool TestSorted();
+    RID GetRid(const int iPos);
+    RID GetNodeRID();
+    int GetOrder();
 private:
-    RC InsertNode();
-    RC DeleteNode();
-    RC MergeNode();
-    RC SplitNode();
-
     char *pData;
     char *keys;//the table of key value according to the attribute
     RID  *rids;//the rids values table
@@ -41,8 +53,12 @@ private:
     AttrType attributeType;
     int attributeLength;
     int order;//the order of the node
+    int keysNum;//the number of keys
+    PageNum  left;//to the left neighbour:pageID
+    PageNum  right; //to the right neighbour:pageID
+
+   
 };
 
 
 #endif	/* IX_BTNODE_H */
-
