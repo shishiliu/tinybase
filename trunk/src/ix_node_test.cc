05cde76f1d317bfda075a3e4de112ae478224e8f
wwtmac@gmail.com <wwtmac@gmail.com@2e8724b3-4663-09c7-7dde-4004479e4a0f>
2013-05-26 11:35:40 +0000

    adding test

--- trunk/src/ix_node_test.cc
+++ trunk/src/ix_node_test.cc
@@ -22,11 +22,78 @@ RC TestInsert(PF_PageHandle &pageHandler) {
    return !aborted? OK_RC: 1;
 }
 
-RC TestFind() {
-   return OK_RC;
+RC TestFind(PF_PageHandle &pageHandler) {
+  int key1 = 1;
+   RID rid1(1, 1);
+    int key2 = 2;
+   RID rid2(2, 2);
+   int key3 = 3;
+   RID rid3(3, 3);
+   IX_BTNode node(INT, sizeof(int), pageHandler, true);
+   node.InsertNode(&key1, rid1);
+   node.InsertNode(&key2, rid2);
+   node.InsertNode(&key3, rid3);
+   const void * test = (void *) (&key2);
+   int position = node.FindKey(test);
+    bool result =  (position == 1);
+      if (!result) {
+      cerr << "Test failed" << endl;
+   }
+   return result? OK_RC: 1;
 }
 
-RC TestDelete() {
+RC TestDelete(PF_PageHandle &pageHandler) {
+   int  key1 = 1;
+   RID rid1(1, 1);
+    int key2 = 2;
+   RID rid2(2, 2);
+   int key3 = 3;
+   RID rid3(3, 3);
+   IX_BTNode node(INT, sizeof(int), pageHandler, true);
+    node.InsertNode(&key1, rid1);
+   node.InsertNode(&key2, rid2);
+   node.InsertNode(&key3, rid3);
+   node.DeleteNode(&key1);
+   void *pResultKey = 0;
+   node.GetKey(0, pResultKey);
+   bool aborted = *static_cast<int*>(pResultKey) != key2;
+    if (aborted) {
+      cerr << "Test failed" << endl;
+   }
+   return !aborted? OK_RC: 1;
+}
+RC TestMerge(PF_PageHandle &pageHandler1,PF_PageHandle &pageHandler2) {
+           IX_BTNode node1(INT, sizeof(int), pageHandler1, true);
+           IX_BTNode node2(INT, sizeof(int), pageHandler2, true);
+     int  key1 = 1;
+     RID rid1(1, 1);
+     int key2 = 3;
+     RID rid2(2, 2);
+     int key3 = 5;
+     RID rid3(3, 3);
+   node1.InsertNode(&key1, rid1);
+   node1.InsertNode(&key2, rid2);
+   node1.InsertNode(&key3, rid3);
+   cout<<node1<<endl;
+   PageNum id1 = node1.GetNodeRID().GetPage();
+   PageNum id2 = node2.GetNodeRID().GetPage();
+   node1.SetRight(id2);
+   node2.SetLeft(id1);
+   int  key4 = 2;
+     RID rid4(1, 1);
+     int key5 = 4;
+     RID rid5(2, 2);
+     int key6 = 6;
+     RID rid6(3, 3);
+     int key7 = 7;
+     RID rid7(4, 4);
+   node2.InsertNode(&key4, rid4);
+   node2.InsertNode(&key5, rid5);
+   node2.InsertNode(&key6, rid6);
+   node2.InsertNode(&key7, rid7); 
+   cout<<node2<<endl;
+   node1.MergeNode(&node2);
+   cout<<node1<<endl;
    return OK_RC;
 }
 
