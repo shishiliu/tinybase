ce2669e9b50dcce0fde52ab4e61658caa2497672
wwtmac@gmail.com <wwtmac@gmail.com@2e8724b3-4663-09c7-7dde-4004479e4a0f>
2013-06-03 09:43:36 +0000

    remove useless folder

--- trunk/trunk/build/rm_filescan.d
+++
@@ -1,2 +0,0 @@
-../build/rm_filescan.o ../build/rm_filescan.d: rm_filescan.cc rm_internal.h rm.h redbase.h \
- rm_rid.h rm_rid.fwd.h pf.h pf.fwd.h rm.fwd.h
--- trunk/trunk/build/rm_manager.d
+++
@@ -1,2 +0,0 @@
-../build/rm_manager.o ../build/rm_manager.d: rm_manager.cc rm_internal.h rm.h redbase.h \
- rm_rid.h rm_rid.fwd.h pf.h pf.fwd.h rm.fwd.h
