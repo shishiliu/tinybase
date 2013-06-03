e913503c910f04c4f2ba633620042f7dfc4fdee4
wwtmac@gmail.com <wwtmac@gmail.com@2e8724b3-4663-09c7-7dde-4004479e4a0f>
2013-06-03 09:30:32 +0000

    index scan cc

---
+++ src/ix_indexscan.cc
@@ -0,0 +1,36 @@
+#include "ix.h"
+
+IX_IndexScan::IX_IndexScan()
+{
+}
+
+IX_IndexScan::~IX_IndexScan()
+{
+}
+// Open index scan
+/*enum CompOp {
+    NO_OP,  
+    EQ_OP, ==
+    NE_OP, !=
+    LT_OP, <
+    GT_OP, >
+    LE_OP, 
+    GE_OP    
+};
+*/
+RC IX_IndexScan::OpenScan(const IX_IndexHandle &indexHandle, CompOp compOp, void *value, ClientHint  pinHint)
+{
+	return 0;
+}
+
+// Get the next matching entry return IX_EOF if no more matching
+// entries.
+RC IX_IndexScan::GetNextEntry(RID &rid)
+{
+	return 0;
+}
+// Close index scan
+RC IX_IndexScan::CloseScan()
+{
+	return 0;
+}
